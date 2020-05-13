aab80cf6a18a87457edcc7c230a033b272bd196d
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2020-05-13 18:15:26 -0400

    Added function to display helpful description when icons are hovered over. Used map function within the jsx to display the icons and the seperator.

--- src/components/IconBar.js
+++ src/components/IconBar.js
@@ -2,18 +2,31 @@ import React, { useState } from 'react';
 import Icon from './Icon';
 
 function IconBar({ icons, toggleHover }) {
-    return (
-      <div id='breadcrumb'>
-        {icons.map((icon) => {
-          if(icon.id !== 3) {
-            return [<Icon key={icon.id} icon={icon} toggleHover={toggleHover}/>, <div key={'sep' + icon.id} id='separator'>/</div>]
-            } else {
-              return <Icon key={icon.id} icon={icon} toggleHover={toggleHover}/>
-            }
-        })} 
+
+  const helpfulUI = () => {
+    // Need to provide a return to the function as a whole
+    return icons.filter((icon) => {
+      return icon.hover === true
+    }).map((icon) => {
+      return <h1 id='description' key={icon.id}>{icon.description}</h1>
+    })
+  }; 
+
+    return ( 
+      <div> 
+        {helpfulUI()}
+        <div id='breadcrumb'>
+          {icons.map((icon) => {
+            if(icon.id !== 3) {
+              // Return the separator but not for the last icon
+              return [<Icon key={icon.id} icon={icon} toggleHover={toggleHover}/>, <div key={'sep' + icon.id} id='separator'>/</div>]
+              } else {
+                return <Icon key={icon.id} icon={icon} toggleHover={toggleHover}/>
+              }
+          })} 
+        </div>
       </div>
     )
-
 }
 
 export default IconBar; 
