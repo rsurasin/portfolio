c9817104a47493a218c92d6e0e4587fa4d828410
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2020-05-16 15:47:01 -0400

    Added a navbar component.

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'; 
+
+function NavBar() {
+    return (
+        <div id='navbar'>
+            <a id='NavItem'>Home</a>
+            <a id='NavItem'>About</a>
+        </div>
+    )
+}
+
+export default NavBar; 
