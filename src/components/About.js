b6a1fa65ec0d8eac337b099a2a55489195ccd0b2
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2020-05-17 23:53:00 -0400

    Added an About component/page for react router to render when in path /about.

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'; 
+import NavBar from './NavBar';
+
+function About() {
+    return (
+        <div>
+            <NavBar />
+            <h1 style={{color: 'white'}}>About Page</h1>
+        </div>
+    )
+}
+
+export default About; 
